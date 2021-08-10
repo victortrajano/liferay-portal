c5aed6929c32dfa22acd6755d7898404d172e2a1
Mateus Castro <mateus.castro.domingos@gmail.com>
2021-08-10 13:32:07 -0300

    LPS-136591 Add limit to input search of 255 characters

--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/components/fragments/Forms/Input/Search.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/components/fragments/Forms/Input/Search.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React from 'react';
 
-import { Label } from "../Label";
-import { InputAreaWithError } from "../InputArea/WithError";
+import {Label} from '../Label';
+import {InputAreaWithError} from '../InputArea/WithError';
 
 export const SearchInput = React.forwardRef(
-  (
-    { name, label, renderActions, children, required = false, error, ...props },
-    ref
-  ) => {
-    return (
-      <InputAreaWithError error={error}>
-        {label && (
-          <Label name={name} label={label} required={required}>
-            {renderActions}
-          </Label>
-        )}
-        <div className="content-row">
-          <input
-            {...props}
-            ref={ref}
-            name={name}
-            required={required}
-            maxLength={256}
-          />
-          {children}
-        </div>
-      </InputAreaWithError>
-    );
-  }
+	(
+		{
+			name,
+			label,
+			renderActions,
+			children,
+			required = false,
+			error,
+			...props
+		},
+		ref
+	) => {
+		return (
+			<InputAreaWithError error={error}>
+				{label && (
+					<Label name={name} label={label} required={required}>
+						{renderActions}
+					</Label>
+				)}
+				<div className="content-row">
+					<input
+						{...props}
+						ref={ref}
+						name={name}
+						required={required}
+						maxLength={255}
+					/>
+					{children}
+				</div>
+			</InputAreaWithError>
+		);
+	}
 );

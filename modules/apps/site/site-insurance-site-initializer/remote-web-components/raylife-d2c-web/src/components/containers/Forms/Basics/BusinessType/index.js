d51165a76cdee00409fdf2fb6b386d7e9b7a3714
Mateus Castro <mateus.castro.domingos@gmail.com>
2021-08-10 13:19:39 -0300

    LPS-136594 Add link to Previous Button

--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/components/containers/Forms/Basics/BusinessType/index.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/components/containers/Forms/Basics/BusinessType/index.js
@@ -1,31 +1,36 @@
-import React from "react";
-import { useFormContext, useWatch } from "react-hook-form";
+import React from 'react';
+import {useFormContext, useWatch} from 'react-hook-form';
 
-import { useStepWizard } from "../../../../../hooks/useStepWizard";
-import { AVAILABLE_STEPS } from "../../../../../utils/constants";
-import { BusinessTypeSearch } from "./Search";
-import { CardFormActions } from "../../../../fragments/Card/FormActions";
+import {useStepWizard} from '../../../../../hooks/useStepWizard';
+import {AVAILABLE_STEPS} from '../../../../../utils/constants';
+import {BusinessTypeSearch} from './Search';
+import {CardFormActions} from '../../../../fragments/Card/FormActions';
 
 export const FormBasicBusinessType = () => {
-  const {
-    formState: { isValid },
-  } = useFormContext();
-  const form = useWatch();
-  const { setSection } = useStepWizard();
+	const {
+		formState: {isValid},
+	} = useFormContext();
+	const form = useWatch();
+	const {setSection} = useStepWizard();
 
-  const goToNextForm = () => {
-    setSection(AVAILABLE_STEPS.BASICS_BUSINESS_INFORMATION);
-  };
+	const goToNextForm = () => {
+		setSection(AVAILABLE_STEPS.BASICS_BUSINESS_INFORMATION);
+	};
 
-  return (
-    <div className="card">
-      <div className="card-content">
-        <BusinessTypeSearch />
-      </div>
-      <CardFormActions
-        onNext={goToNextForm}
-        isValid={isValid && form?.basics?.properties?.businessClassCode}
-      />
-    </div>
-  );
+	const goToPreviousPage = () => {
+		window.history.back();
+	};
+
+	return (
+		<div className="card">
+			<div className="card-content">
+				<BusinessTypeSearch />
+			</div>
+			<CardFormActions
+				onNext={goToNextForm}
+				onPrevious={goToPreviousPage}
+				isValid={isValid && form?.basics?.businessClassCode}
+			/>
+		</div>
+	);
 };

237fee14dc8643e1607ee3ffbfa006fcabe98ffd
Keven Leone <keven.leone@liferay.com>
2021-08-12 09:05:26 -0300

    LPS-137025 Add missing property

--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/components/containers/Forms/Basics/BusinessType/index.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/components/containers/Forms/Basics/BusinessType/index.js
@@ -29,7 +29,7 @@ export const FormBasicBusinessType = () => {
 			<CardFormActions
 				onNext={goToNextForm}
 				onPrevious={goToPreviousPage}
-				isValid={isValid && form?.basics?.businessClassCode}
+				isValid={isValid && form?.basics?.properties?.businessClassCode}
 			/>
 		</div>
 	);

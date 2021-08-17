d14512c46c2cc444f4d9a6a7b9559ea7080a1c52
Giuseppe Davanzo <giuseppe.davanzo@objective.com.br>
2021-08-17 12:28:02 -0300

    LPS-137219 Configure clay icons

--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/Providers.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/Providers.js
@@ -1,17 +1,32 @@
-import React from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import React from 'react';
+import {FormProvider, useForm} from 'react-hook-form';
+import {ClayIconSpriteContext} from '@clayui/icon';
 
-import { AppProvider } from "./context/AppContext";
-import { Template } from "./components/Template";
+import {AppProvider} from './context/AppContext';
+import {Template} from './components/Template';
 
-export const Providers = ({ children }) => {
-  const form = useForm({ mode: "onChange" });
+const getIconSpriteMap = () => {
+	try {
+		// eslint-disable-next-line no-undef
+		if (!themeDisplay) new Error('themeDisplay is not defined');
+		// eslint-disable-next-line no-undef
+		return `${themeDisplay.getPathThemeImages()}/clay/icons.svg`;
+	} catch (err) {
+		console.warn(err.message);
+		return require('@clayui/css/lib/images/icons/icons.svg').default;
+	}
+};
 
-  return (
-    <AppProvider>
-      <FormProvider {...form}>
-        <Template>{children}</Template>
-      </FormProvider>
-    </AppProvider>
-  );
+export const Providers = ({children}) => {
+	const form = useForm({mode: 'onChange'});
+
+	return (
+		<AppProvider>
+			<ClayIconSpriteContext.Provider value={getIconSpriteMap()}>
+				<FormProvider {...form}>
+					<Template>{children}</Template>
+				</FormProvider>
+			</ClayIconSpriteContext.Provider>
+		</AppProvider>
+	);
 };
--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/index.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/index.js
@@ -1,35 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { App } from "./App";
-import { Providers } from "./Providers";
-import StylesProvider from "./styles/provider.scss";
+import {App} from './App';
+import {Providers} from './Providers';
+import StylesProvider from './styles/provider.scss';
 
-const TAG_NAME = "d2c-web";
+const TAG_NAME = 'd2c-web';
 
 class WebComponent extends HTMLElement {
-  constructor() {
-    super();
-    this.styleSass = document.createElement("style");
-    this.mountPoint = document.createElement("div");
+	constructor() {
+		super();
+		this.styleSass = document.createElement('style');
+		this.mountPoint = document.createElement('div');
 
-    this.attachShadow({ mode: "open" });
-  }
+		this.attachShadow({mode: 'open'});
+	}
 
-  connectedCallback() {
-    this.styleSass.textContent = StylesProvider;
-    this.shadowRoot.appendChild(this.styleSass);
-    this.shadowRoot.appendChild(this.mountPoint);
+	connectedCallback() {
+		this.styleSass.textContent = StylesProvider;
+		this.shadowRoot.appendChild(this.styleSass);
+		this.shadowRoot.appendChild(this.mountPoint);
 
-    ReactDOM.render(
-      <Providers>
-        <App />
-      </Providers>,
-      this.mountPoint
-    );
-  }
+		ReactDOM.render(
+			<Providers>
+				<App />
+			</Providers>,
+			this.mountPoint
+		);
+	}
 }
 
 if (!customElements.get(TAG_NAME)) {
-  customElements.define(TAG_NAME, WebComponent);
+	customElements.define(TAG_NAME, WebComponent);
 }

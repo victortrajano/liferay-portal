d1df82639cc0594780b639716f4d7e97210da9f6
Jair Medeiros <jair.medeiros@liferay.com>
2021-08-18 11:55:55 -0300

    LPS-134818 Add method to convert string to Slug pattern

--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/utils/index.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/utils/index.js
@@ -45,3 +45,22 @@ export const calculateCircumference = (radius) => radius * 2 * Math.PI;
  */
 export const calculateOffset = (percent, circumference) =>
   circumference - (percent / 100) * circumference;
+
+
+export const toSlug = (str) => {
+  str = str.replace(/^\s+|\s+$/g, '');
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to = "aaaaeeeeiiiioooouuuunc------";
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-');
+
+  return str;
+}

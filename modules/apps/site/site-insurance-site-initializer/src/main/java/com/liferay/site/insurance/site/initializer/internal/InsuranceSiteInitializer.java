6eed37300f292bfa8ced4d9bb3cf3c13dbf5f91e
Jose Abelenda <jose.abelenda@liferay.com>
2021-08-19 15:46:20 -0300

    LPS-137595 Fix Liferay Object Definition, remove space

--- modules/apps/site/site-insurance-site-initializer/src/main/java/com/liferay/site/insurance/site/initializer/internal/InsuranceSiteInitializer.java
+++ modules/apps/site/site-insurance-site-initializer/src/main/java/com/liferay/site/insurance/site/initializer/internal/InsuranceSiteInitializer.java
@@ -707,7 +707,7 @@ public class InsuranceSiteInitializer implements SiteInitializer {
 			_objectDefinitionLocalService.addCustomObjectDefinition(
 				user.getUserId(),
 				LocalizedMapUtil.getLocalizedMap("Raylife Application"),
-				"Raylife Application",
+				"RaylifeApplication",
 				LocalizedMapUtil.getLocalizedMap("Raylife Applications"),
 				Arrays.asList(
 					ObjectFieldUtil.createObjectField(

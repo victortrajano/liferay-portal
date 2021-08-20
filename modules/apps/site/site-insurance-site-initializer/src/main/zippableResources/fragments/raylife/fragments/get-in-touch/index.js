9a27b62796ed5a6124831b06c2010a1e325b12b3
Rodrigo Cavalcanti <rodrigo.cavalcanti@liferay.com>
2021-08-20 11:04:25 -0300

    LPS-137518 Disable click to chat and create button to previous screen

--- modules/apps/site/site-insurance-site-initializer/src/main/zippableResources/fragments/raylife/fragments/get-in-touch/index.js
+++ modules/apps/site/site-insurance-site-initializer/src/main/zippableResources/fragments/raylife/fragments/get-in-touch/index.js
@@ -1,5 +1,10 @@
+const btnBack   = fragmentElement.querySelector("#contact-agent-btn-back");
 const btnCall   = fragmentElement.querySelector("#contact-agent-btn-call");
 const valueCall = fragmentElement.querySelector("#value-number-call").textContent;
+
+btnBack.onclick = function(){
+	window.history.back();
+}
 
 btnCall.onclick = function(){
 	window.location.href = 'tel:' + valueCall;

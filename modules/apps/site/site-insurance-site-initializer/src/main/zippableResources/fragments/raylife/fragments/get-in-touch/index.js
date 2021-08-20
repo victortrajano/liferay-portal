5326e9e90e99a6b7454b211836e4bb5fe9911327
Rodrigo Cavalcanti <rodrigo.cavalcanti@liferay.com>
2021-08-20 14:36:51 -0300

    LPS-137740 Get Application ID by cookie

--- modules/apps/site/site-insurance-site-initializer/src/main/zippableResources/fragments/raylife/fragments/get-in-touch/index.js
+++ modules/apps/site/site-insurance-site-initializer/src/main/zippableResources/fragments/raylife/fragments/get-in-touch/index.js
@@ -1,6 +1,11 @@
-const btnBack   = fragmentElement.querySelector("#contact-agent-btn-back");
-const btnCall   = fragmentElement.querySelector("#contact-agent-btn-call");
-const valueCall = fragmentElement.querySelector("#value-number-call").textContent;
+let   applicationId = "";
+let   auxCookie     = "";
+const decodedCookie = decodeURIComponent(document.cookie);
+const infoCookie 	= decodedCookie.split(';');
+const nameCookie 	= "raylife-application-id=";
+const btnBack       = fragmentElement.querySelector("#contact-agent-btn-back");
+const btnCall       = fragmentElement.querySelector("#contact-agent-btn-call");
+const valueCall     = fragmentElement.querySelector("#value-number-call").textContent;
 
 btnBack.onclick = function(){
 	window.history.back();
@@ -8,4 +13,15 @@ btnBack.onclick = function(){
 
 btnCall.onclick = function(){
 	window.location.href = 'tel:' + valueCall;
+}
+
+for(let i = 0; i < infoCookie.length; i++) {
+	auxCookie = infoCookie[i];
+	while (auxCookie.charAt(0) == ' ') {
+		auxCookie = auxCookie.substring(1);
+	}
+	if (auxCookie.indexOf(nameCookie) == 0) {
+		applicationId = auxCookie.substring(nameCookie.length, auxCookie.length);
+		document.getElementById("content-agent-text-your-application").textContent = "Your Application #"+applicationId;
+	}
 }

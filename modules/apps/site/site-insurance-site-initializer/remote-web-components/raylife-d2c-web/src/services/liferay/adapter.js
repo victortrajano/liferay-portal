58c293200b1d061f33ca4d019fc4ff1ab13e1ae7
Wellington Barbosa <welitonpe@gmail.com>
2021-08-24 14:29:14 -0300

    LPS-137519 Remove Unused types and adapters

--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/services/liferay/adapter.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/services/liferay/adapter.js
@@ -1,32 +1,60 @@
-import "../../types";
-import { toSlug } from "../../utils";
-import { allowedProductQuote } from "../../utils/webContents";
+import '../../types';
+import {toSlug} from '../../utils';
+import {allowedProductQuote} from '../../utils/webContents';
+
 /**
  * @param {AssetCategoryResponse[]}  data Array of matched categories
  * @returns {BusinessType[]} Array of business types
  */
 const adaptToBusinessType = (data = []) =>
-  data.map(({ categoryId, titleCurrentValue, descriptionCurrentValue }) => ({
-    id: categoryId,
-    title: titleCurrentValue,
-    description: descriptionCurrentValue,
-  }));
+	data.map(({categoryId, titleCurrentValue, descriptionCurrentValue}) => ({
+		description: descriptionCurrentValue,
+		id: categoryId,
+		title: titleCurrentValue,
+	}));
 
 /**
- * @param {BasicsForm}  data Basics form object
+ * @param {DataForm}  data Basics form object
  * @returns {BasicsFormApplicationRequest} Basics Form ready for application request
  */
-const adaptToBasicsFormApplicationRequest = ({ businessInformation }) => ({
-  address: businessInformation.business.location.address,
-  addressApt: businessInformation.business.location.addressApt,
-  city: businessInformation.business.location.city,
-  state: businessInformation.business.location.state,
-  zip: businessInformation.business.location.zip,
-  firstName: businessInformation.firstName,
-  lastName: businessInformation.lastName,
-  email: businessInformation.business.email,
-  phone: businessInformation.business.phone,
-  website: businessInformation.business.website,
+const adaptToFormApplicationRequest = (form) => ({
+	address: form?.basics?.businessInformation?.business?.location?.address,
+	addressApt:
+		form?.basics?.businessInformation?.business?.location?.addressApt,
+	city: form?.basics?.businessInformation?.business?.location?.city,
+	state: form?.basics?.businessInformation?.business?.location?.state,
+	zip: form?.basics?.businessInformation?.business?.location?.zip,
+	firstName: form?.basics?.businessInformation?.firstName,
+	lastName: form?.basics?.businessInformation?.lastName,
+	email: form?.basics?.businessInformation?.business?.email,
+	phone: form?.basics?.businessInformation?.business?.phone,
+	website: form?.basics?.businessInformation?.business?.website,
+
+	hasAutoPolicy: form?.business?.hasAutoPolicy,
+	hasSellProductsUnderOwnBrand: form?.business?.hasSellProductsUnderOwnBrand,
+	hasStoredCustomerInformation: form?.business?.hasStoredCustomerInformation,
+	legalEntity: form?.business?.legalEntity,
+	overallSales: form?.business?.overallSales,
+	salesMerchandise: form?.business?.salesMerchandise,
+	yearsOfExperience: form?.business?.yearsOfExperience,
+
+	annualPayrollForEmployees: form?.employees?.annualPayrollForEmployees,
+	annualPayrollForOwner: form?.employees?.annualPayrollForOwner,
+	businessOperatesYearRound: form?.employees?.businessOperatesYearRound,
+	estimatedAnnualGrossRevenue: form?.employees?.estimatedAnnualGrossRevenue,
+	fein: form?.employees?.fein,
+	hasFein: form?.employees?.hasFein,
+	partTimeEmployees: form?.employees?.partTimeEmployees,
+	startBusinessAtYear: form?.employees?.startBusinessAtYear,
+
+	buildingSquareFeetOccupied: form?.property?.buildingSquareFeetOccupied,
+	doOwnBuildingAtAddress: form?.property?.doOwnBuildingAtAddress,
+	isPrimaryBusinessLocation: form?.property?.isPrimaryBusinessLocation,
+	isThereDivingBoards: form?.property?.isThereDivingBoards,
+	isThereSwimming: form?.property?.isThereSwimming,
+	stories: form?.property?.stories,
+	totalBuildingSquareFeet: form?.property?.totalBuildingSquareFeet,
+	yearBuilding: form?.property?.yearBuilding,
 });
 
 /**
@@ -45,21 +73,21 @@ const adaptToBasicsFormApplicationRequest = ({ businessInformation }) => ({
  * @returns {ProductQuote[]} Array of business types
  */
 const adaptToProductQuote = (data = []) =>
-  data.map(({ productId, name, description, skus }) => ({
-    id: productId,
-    title: name.en_US,
-    description: description.en_US,
-    period: `($${skus[0].promoPrice.toFixed(2).toString()}-${skus[0].price
-      .toFixed(2)
-      .toString()}/mo)`,
-    template: {
-      name: toSlug(name.en_US),
-      allowed: allowedProductQuote(name.en_US),
-		}
-  }));
+	data.map(({productId, name, description, skus}) => ({
+		id: productId,
+		title: name.en_US,
+		description: description.en_US,
+		period: `($${skus[0].promoPrice
+			.toFixed(2)
+			.toString()}-${skus[0].price.toFixed(2).toString()}/mo)`,
+		template: {
+			name: toSlug(name.en_US),
+			allowed: allowedProductQuote(name.en_US),
+		},
+	}));
 
 export const LiferayAdapt = {
-  adaptToBusinessType,
-  adaptToBasicsFormApplicationRequest,
-  adaptToProductQuote,
+	adaptToBusinessType,
+	adaptToProductQuote,
+	adaptToFormApplicationRequest,
 };
--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/types.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/types.js
@@ -49,6 +49,20 @@
 
 /**
  * @typedef {{
+ * firstName: string
+ * lastName: string
+ * phone: string
+ * website: string
+ * address: string
+ * addressApt: string
+ * city: string
+ * state: string
+ * zip: string
+ * }} BasicsFormApplicationRequest
+ */
+
+/**
+ * @typedef {{
  * applicationId: number
  * businessSearch: string
  * businessCategoryId: string
@@ -74,6 +88,67 @@
  *  }
  * }
  * }} BasicsForm
+ */
+
+// TODO COMPLETO
+
+/**
+ * @typedef {{
+ * applicationId: number
+ * businessCategoryId: string
+ * businessSearch: string
+ * ProductQuote: number
+ * properties: {
+ *  businessClassCode: string
+ *  naics: string
+ *  segment: string
+ * }
+ * businessInformation: {
+ *  firstName: string
+ *  lastName: string
+ *  business: {
+ *    email: string
+ *    website: string
+ *    phone: string
+ *    location: {
+ *      address: string
+ *      addressApt: string
+ *      city: string
+ *      state: string
+ *      zip: string
+ *    }
+ *  }
+ * }
+ * business:{
+ *	hasAutoPolicy: string
+ *	hasSellProductsUnderOwnBrand: string
+ *	hasStoredCustomerInformation: string
+ *	legalEntity: string
+ *	overallSales: string
+ *	salesMerchandise: string
+ *	yearsOfExperience: string
+ *  }
+ * employees: {
+ *  annualPayrollForEmployees: string
+ *  annualPayrollForOwner: string
+ *  businessOperatesYearRound: string
+ *  estimatedAnnualGrossRevenue: string
+ *  fein: string
+ *  hasFein: string
+ *  partTimeEmployees: string
+ *  startBusinessAtYear: string
+ * }
+ * property:{
+ *  buildingSquareFeetOccupied: string
+ *  doOwnBuildingAtAddress: string
+ *  isPrimaryBusinessLocation: string
+ *  isThereDivingBoards: string
+ *  isThereSwimming: string
+ *  stories: string
+ *  totalBuildingSquareFeet: string
+ *  yearBuilding: string
+ * }
+ * }} DataForm
  */
 
 /**

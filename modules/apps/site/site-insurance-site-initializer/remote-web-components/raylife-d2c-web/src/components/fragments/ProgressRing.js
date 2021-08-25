b347f6404dd3060ab38d013f59a6c0246acae1da
Mateus Castro <mateus.castro.domingos@gmail.com>
2021-08-25 13:51:43 -0300

    LPS-137855 Fix Step Counter

--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/components/fragments/ProgressRing.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/components/fragments/ProgressRing.js
@@ -1,37 +1,38 @@
-import React from "react";
-import { calculateCircumference, calculateOffset } from "../../utils";
+import React from 'react';
+import {calculateCircumference, calculateOffset} from '../../utils';
 
 export const ProgressRing = ({
-  className,
-  strokeColor = "#4C85FF",
-  strokeWidth = 2,
-  diameter = 24,
-  percent = 0,
+	className,
+	strokeColor = '#4C85FF',
+	strokeWidth = 2,
+	diameter = 24,
+	percent = 0,
 }) => {
-  const radius = diameter / 2;
-  const normalizedRadius = radius - strokeWidth * 2;
+	const radius = diameter / 2;
+	const normalizedRadius = radius - strokeWidth * 2;
+	const center = (radius - strokeWidth) / 2;
 
-  return (
-    <svg className={className} width={diameter} height={diameter}>
-      <circle
-        className="progress"
-        fill="transparent"
-        stroke={strokeColor}
-        strokeWidth={strokeWidth}
-        strokeLinecap="round"
-        r={normalizedRadius}
-        cx={radius}
-        cy={radius}
-        style={{
-          strokeDasharray: `${calculateCircumference(
-            normalizedRadius
-          )} ${calculateCircumference(normalizedRadius)}`,
-          strokeDashoffset: calculateOffset(
-            percent,
-            calculateCircumference(normalizedRadius)
-          ),
-        }}
-      />
-    </svg>
-  );
+	return (
+		<svg className={className} width={diameter} height={diameter}>
+			<circle
+				className="progress"
+				fill="transparent"
+				stroke={strokeColor}
+				strokeWidth={strokeWidth}
+				strokeLinecap="round"
+				r={normalizedRadius}
+				cx={center}
+				cy={center}
+				style={{
+					strokeDasharray: `${calculateCircumference(
+						normalizedRadius
+					)} ${calculateCircumference(normalizedRadius)}`,
+					strokeDashoffset: calculateOffset(
+						percent,
+						calculateCircumference(normalizedRadius)
+					),
+				}}
+			/>
+		</svg>
+	);
 };
--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/utils/constants.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/utils/constants.js
@@ -1,38 +1,38 @@
 export const TOTAL_OF_FIELD = {
-  BASICS: 11,
-  EMPLOYEES: 7,
-  PROPERTY: 6,
+	BASICS: 14,
+	EMPLOYEES: 7,
+	PROPERTY: 6,
 };
 
 export const AVAILABLE_STEPS = {
-  BASICS_BUSINESS_TYPE: {
-    title: "Welcome! Let's start.",
-    section: "basics",
-    subsection: "business-type",
-  },
-  BASICS_BUSINESS_INFORMATION: {
-    title: "Just the business basics!",
-    section: "basics",
-    subsection: "businessInformation",
-  },
-  BASICS_PRODUCT_QUOTE: {
-    title: "Select a product.",
-    section: "basics",
-    subsection: "product-quote",
-  },
-  BUSINESS: {
-    title: "Let's get to know your business!",
-    section: "business",
-    subsection: "",
-  },
-  EMPLOYEES: {
-    title: "Tell us about your employees!",
-    section: "employees",
-    subsection: "",
-  },
-  PROPERTY: {
-    title: "More about",
-    section: "property",
-    subsection: "",
-  },
+	BASICS_BUSINESS_TYPE: {
+		title: "Welcome! Let's start.",
+		section: 'basics',
+		subsection: 'business-type',
+	},
+	BASICS_BUSINESS_INFORMATION: {
+		title: 'Just the business basics!',
+		section: 'basics',
+		subsection: 'businessInformation',
+	},
+	BASICS_PRODUCT_QUOTE: {
+		title: 'Select a product.',
+		section: 'basics',
+		subsection: 'product-quote',
+	},
+	BUSINESS: {
+		title: "Let's get to know your business!",
+		section: 'business',
+		subsection: '',
+	},
+	EMPLOYEES: {
+		title: 'Tell us about your employees!',
+		section: 'employees',
+		subsection: '',
+	},
+	PROPERTY: {
+		title: 'More about',
+		section: 'property',
+		subsection: '',
+	},
 };

7767554c4d6d0a1196e2ed51decac7c7370b48b2
Keven Leone <keven.leone@liferay.com>
2021-09-10 16:20:01 -0300

    LPS-139094 Update Events

--- modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/events.js
+++ modules/apps/site/site-insurance-site-initializer/remote-web-components/raylife-d2c-web/src/events.js
@@ -1,2 +1,2 @@
-export const TIP_EVENT = 'raylife-fragment-interactive';
-export const TIP_EVENT_DISMISS = 'raylife-fragment-dismissed';
+export const TIP_EVENT = 'raylife-tip-container-primary';
+export const TIP_EVENT_DISMISS = 'raylife-tip-container-dismiss-primary';

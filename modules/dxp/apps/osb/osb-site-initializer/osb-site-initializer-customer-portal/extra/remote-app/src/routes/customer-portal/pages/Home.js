644ed710b67f1e1b7ee1541043737a139a9caa0d
Aguinus Santana <aguinus.santana@liferay.com>
2021-11-08 16:43:16 -0300

    LPS-140155 Banner and home page adjustments.

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Home.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/pages/Home.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import useGraphQL from '~/common/hooks/useGraphql';
 import { LiferayTheme } from '~/common/services/liferay';
 import { getKoroneikiAccountsByFilter } from '~/common/services/liferay/graphql/koroneiki-accounts';
@@ -9,97 +9,78 @@ import { REACT_APP_LIFERAY_API } from '~/common/utils';
 import Banner from '../components/Banner';
 import ProjectCard from '../components/ProjectCard';
 import SearchProject from '../components/SearchProject';
-import { AppContext } from "../context";
 import { status } from '../utils/constants';
 
 const PROJECT_THRESHOLD_COUNT = 4;
 
 const Home = () => {
+	const [keyword, setKeyword] = useState('');
 	const { data: userAccount } = useGraphQL(getUserAccountById(LiferayTheme.getUserId()));
-	const [{ assetsPath }] = useContext(AppContext);
 
 	useEffect(() => {
 		if (userAccount) {
-			Storage.setItem(
-				STORAGE_KEYS.USER_APPLICATION,
-				JSON.stringify({
-					image:
-						userAccount.image &&
-						`${REACT_APP_LIFERAY_API}${userAccount.image}`,
-					name: userAccount.name,
-				})
-			);
-		}
-		else {
+			Storage.setItem(STORAGE_KEYS.USER_APPLICATION, JSON.stringify({
+				accountKey: userAccount.accountKey,
+				image: userAccount.image && `${REACT_APP_LIFERAY_API}${userAccount.image}`,
+				name: userAccount.name
+			}));
+		} else {
 			Storage.removeItem(STORAGE_KEYS.USER_APPLICATION);
 		}
 	}, [userAccount]);
 
 	const accountBriefs = userAccount?.accountBriefs || [];
 
-	const { data: koroneikiAccounts } =
-		useGraphQL(
-			getKoroneikiAccountsByFilter({
-				accountKeys: accountBriefs.map(
-					({ externalReferenceCode }) => externalReferenceCode
-				),
-			})
-		) || [];
+	const { data: koroneikiAccounts } = useGraphQL(
+		getKoroneikiAccountsByFilter({
+			accountKeys: accountBriefs.map(
+				({ externalReferenceCode }) => externalReferenceCode
+			)
+		})
+	) || [];
 
-	const projects =
-		koroneikiAccounts?.map(
-			({
-				accountKey,
-				liferayContactEmailAddress,
-				liferayContactName,
-				liferayContactRole,
-				region,
-				slaCurrent,
-				slaCurrentEndDate,
-			}) => ({
-				contact: {
-					emailAddress: liferayContactEmailAddress,
-					name: liferayContactName,
-					role: liferayContactRole,
-				},
-				region,
-				sla: {
-					current: slaCurrent,
-					currentEndDate: slaCurrentEndDate,
-				},
-				status: slaCurrent ? status.active : status.expired,
-				title: accountBriefs.find(
-					({ externalReferenceCode }) =>
-						externalReferenceCode === accountKey
-				).name,
-			})
-		) || [];
+	const getStatus = (slaCurrent, slaExpired, slaFuture) => {
+		if (slaCurrent.length) {
+			return status.active
+		} else if (slaExpired.length) {
+			return status.expired
+		} else if (slaFuture.length) {
+			return status.future
+		}
+	}
 
-	const [currentProjects, setProjects] = useState(projects);
-	const changeInput = (value) => setProjects(projects.filter((project) => project.title.toLowerCase().includes(value.toLowerCase())));
+	const projects = koroneikiAccounts?.map(
+		({ accountKey, code, liferayContactEmailAddress, liferayContactName, liferayContactRole, region, slaCurrent, slaCurrentEndDate, slaExpired, slaFuture }) => ({
+			code,
+			contact: {
+				emailAddress: liferayContactEmailAddress,
+				name: liferayContactName,
+				role: liferayContactRole
+			},
+			region,
+			sla: {
+				current: slaCurrent,
+				currentEndDate: slaCurrentEndDate,
+				expired: slaExpired,
+				future: slaFuture,
+			},
+			status: getStatus(slaCurrent, slaExpired, slaFuture),
+			title: accountBriefs.find(({ externalReferenceCode }) => externalReferenceCode === accountKey).name
+		})) || [];
+
+	const projectsFiltered = projects
+		.filter((project) => keyword ? project.title.toLowerCase().includes(keyword.toLowerCase()) : true)
+	const nextPage = (project) => Storage.setItem(STORAGE_KEYS.KORONEIKI_APPLICATION, JSON.stringify(project));
 	const withManyProjects = projects.length > PROJECT_THRESHOLD_COUNT;
-
-	const nextPage = (project) => {
-		Storage.setItem(
-			STORAGE_KEYS.KORONEIKI_APPLICATION,
-			JSON.stringify(project)
-		);
-	};
 
 	return (
 		<>
-			<div className="d-flex flex-column ml-2 pl-5">
-				<div
-					className={classNames('display-4 font-weight-bold mb-5', {
-						'pb-2': withManyProjects
-					})}
-				>
-					<Banner />
-
-					<img
-						src={`${assetsPath}/teste/banner_background_customer_portal.svg`}
-					/>
-				</div>
+			<div
+				className={classNames('mb-5 mt-5', {
+					'pb-2': withManyProjects
+				})}
+			>
+				<Banner userName={userAccount?.name || ''} />
 			</div>
 			<div className={classNames("mx-auto", {
 				"col-5": withManyProjects,
@@ -107,21 +88,22 @@ const Home = () => {
 			})}>
 				<div className="d-flex flex-column">
 					{withManyProjects && (
-						<div className="align-items-center d-flex justify-content-between mb-4 mr-5 pr-3">
-							<SearchProject onChange={changeInput} />
+						<div className="align-items-center d-flex justify-content-between mb-4 mr-5">
+							<SearchProject onChange={setKeyword} value={keyword} />
 
 							<h5 className="m-0 text-neutral-3">
 								{projects.length} projects
 							</h5>
 						</div>
 					)}
+
 					<div
 						className={classNames('d-flex flex-wrap', {
 							"home-projects": !withManyProjects,
 							"home-projects-sm pt-2": withManyProjects,
 						})}
 					>
-						{currentProjects.map((project, index) => (
+						{projectsFiltered.map((project, index) => (
 							<ProjectCard
 								isSmall={withManyProjects}
 								key={index}

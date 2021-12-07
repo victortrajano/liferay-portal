ee6b94d5b89b0762325aafd2fec27fade8cf6224
LucasAcacio <lucas.acacio@liferay.com>
2021-12-07 18:17:06 -0300

    LPS-146487 adding kanban json resources

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -1618,8 +1618,6 @@ public class BundleSiteInitializer implements SiteInitializer {
 			HashMapBuilder.putAll(
 				assetListEntryIdsStringUtilReplaceValues
 			).putAll(
-				formInstanceIdsStringUtilReplaceValues
-			).putAll(
 				documentsStringUtilReplaceValues
 			).putAll(
 				remoteAppEntryIdsStringUtilReplaceValues

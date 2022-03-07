17efb8f5fcf4fe0d4341b6a1b00e322e690ac836
Jair Medeiros <jair.medeiros@liferay.com>
2022-03-07 16:49:41 -0300

    LPS-146811 Add custom query for accountSubscriptionGroups

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/graphql/account-subscription-groups/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/graphql/account-subscription-groups/index.js
@@ -10,4 +10,5 @@
  */
 
 export * from './queries/useGetAccountSubscriptionGroups';
+export * from './custom-queries/useGetAccountSubscriptionGroupsByAccountKey';
 export * from './mutations/useUpdateAccountSubscriptionGroup';

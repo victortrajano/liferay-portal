28be8905b565fef068ca032c058c20b0812faf4c
Jair Medeiros <jair.medeiros@liferay.com>
2022-03-07 16:50:21 -0300

    LPS-146811 Update typePolicy for StructuredContent

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/graphql/structured-contents/typePolicy.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/graphql/structured-contents/typePolicy.js
@@ -14,6 +14,6 @@ export const structuredContentsTypePolicy = {
 		keyFields: ['contentTemplateId'],
 	},
 	StructuredContent: {
-		keyFields: ['externalReferenceCode'],
+		keyFields: ['id'],
 	},
 };

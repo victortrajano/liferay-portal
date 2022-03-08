a114af9fa4e3e608541ecd792245b619a94d8afa
Jair Medeiros <jair.medeiros@liferay.com>
2022-03-08 05:41:37 -0300

    LPS-146811 Fix conditionals to return loading state

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/QuickLinksPanel/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/QuickLinksPanel/index.js
@@ -87,75 +87,70 @@ const QuickLinksPanel = ({contents}) => {
 		]
 	);
 
+	if (loading || !quickLinksContents?.length) {
+		return <QuickLinksSkeleton />;
+	}
+
 	return (
-		<>
-			{loading || quickLinksContents?.length ? (
-				<div
+		<div
+			className={classNames(
+				'cp-link-body quick-links-container rounded',
+				{
+					'p-4': isQuickLinksExpanded,
+					'position-absolute px-3 py-4': !isQuickLinksExpanded,
+				}
+			)}
+		>
+			<div className="align-items-center d-flex justify-content-between">
+				<h5 className="m-0 text-neutral-10">Quick Links</h5>
+
+				<a
 					className={classNames(
-						'cp-link-body quick-links-container rounded',
+						'btn font-weight-bold p-2 text-neutral-8 text-paragraph-sm',
 						{
-							'p-4': isQuickLinksExpanded,
-							'position-absolute px-3 py-4': !isQuickLinksExpanded,
+							'pl-3': !isQuickLinksExpanded,
 						}
 					)}
-				>
-					<div className="align-items-center d-flex justify-content-between">
-						<h5 className="m-0 text-neutral-10">Quick Links</h5>
+					onClick={() =>
+						setIsQuickLinksExpanded(
+							(previousIsQuickLinksExpanded) => {
+								storage.setItem(
+									STORAGE_KEYS.quickLinksExpanded,
+									JSON.stringify(
+										!previousIsQuickLinksExpanded
+									)
+								);
 
-						<a
-							className={classNames(
-								'btn font-weight-bold p-2 text-neutral-8 text-paragraph-sm',
-								{
-									'pl-3': !isQuickLinksExpanded,
-								}
-							)}
-							onClick={() =>
-								setIsQuickLinksExpanded(
-									(previousIsQuickLinksExpanded) => {
-										storage.setItem(
-											STORAGE_KEYS.quickLinksExpanded,
-											JSON.stringify(
-												!previousIsQuickLinksExpanded
-											)
-										);
-
-										return !previousIsQuickLinksExpanded;
-									}
-								)
+								return !previousIsQuickLinksExpanded;
 							}
-						>
-							<ClayIcon
-								className="mr-1"
-								symbol={isQuickLinksExpanded ? 'hr' : 'plus'}
-							/>
+						)
+					}
+				>
+					<ClayIcon
+						className="mr-1"
+						symbol={isQuickLinksExpanded ? 'hr' : 'plus'}
+					/>
 
-							{isQuickLinksExpanded ? 'Hide' : ''}
-						</a>
-					</div>
+					{isQuickLinksExpanded ? 'Hide' : ''}
+				</a>
+			</div>
 
-					{isQuickLinksExpanded && (
-						<div>
-							{quickLinksContents.map((quickLinkContent) => (
-								<div
-									className="bg-white cp-link-body my-3 p-3 quick-links-card rounded-lg"
-									dangerouslySetInnerHTML={{
-										__html: DOMPurify.sanitize(
-											quickLinkContent,
-											{
-												USE_PROFILES: {html: true},
-											}
-										),
-									}}
-									key={quickLinkContent}
-								></div>
-							))}
-						</div>
-					)}
+			{isQuickLinksExpanded && (
+				<div>
+					{quickLinksContents.map((quickLinkContent) => (
+						<div
+							className="bg-white cp-link-body my-3 p-3 quick-links-card rounded-lg"
+							dangerouslySetInnerHTML={{
+								__html: DOMPurify.sanitize(quickLinkContent, {
+									USE_PROFILES: {html: true},
+								}),
+							}}
+							key={quickLinkContent}
+						></div>
+					))}
 				</div>
-			) : (
-				<QuickLinksSkeleton />
 			)}
-		</>
+		</div>
 	);
 };
 
